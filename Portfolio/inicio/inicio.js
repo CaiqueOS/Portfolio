964f8dcab1730604a3bcaa3660f7014eff621415
Caique Oliveira <78798335+CaiqueOS@users.noreply.github.com>
2022-06-18 11:25:13 -0300

    Delete Portfolio directory

--- Portfolio/inicio/inicio.js
+++
@@ -1,5 +0,0 @@
-function abrirTela(){
-    window.location.assign("../portfolio/index.html");
-}
-
-console.log("Clique na tela");
--- Portfolio/script/script.js
+++
@@ -1,9 +0,0 @@
-function abrirTela(){
-    window.location.assign("./portfolio.html");
-}
-
-setTimeout(function() {
-    window.location.href = "./portfolio.html";
-}, 5000);
-
-console.log("Clique na tela");
