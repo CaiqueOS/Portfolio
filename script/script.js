6e038abbb1c8b1ae04a669dc88eb3142111f8cf2
Caique Oliveira <78798335+CaiqueOS@users.noreply.github.com>
2022-06-18 11:24:48 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function abrirTela(){
+    window.location.assign("./portfolio.html");
+}
+
+setTimeout(function() {
+    window.location.href = "./portfolio.html";
+}, 5000);
+
+console.log("Clique na tela");
